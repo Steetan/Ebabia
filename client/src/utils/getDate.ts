6f58c1a0ea2a689c9fab2339db49af5dfe04a6c8
Steetan <vlad.no.96@bk.ru>
2025-08-21 21:16:33 +0300

    fix date

--- client/src/utils/getDate.ts
+++ client/src/utils/getDate.ts
@@ -7,5 +7,5 @@ export const getDate = () => {
 	const hours = date.getHours() >= 10 ? date.getHours() : `0${date.getHours()}`
 	const minutes = date.getMinutes() >= 10 ? date.getMinutes() : `0${date.getMinutes()}`
 
-	return `${day}.${month}.${year} ${hours}:${minutes}`
+	return `$${year}-${month}-${day} ${hours}:${minutes}`
 }
